74f9d867e32ec7c1b9f73ca0d606806450cba9e1
Manthan Acharya <manthan.boston@gmail.com>
2022-09-01 00:24:25 +0000

    add docker dev container config files

--- svelte/src/admin/ScoutList.svelte
+++ svelte/src/admin/ScoutList.svelte
@@ -30,6 +30,17 @@
 
     const toggle = (scout) => {
         scout.enabled = !scout.enabled;
+
+        // hover over the button to see the color change
+        // if scout is enabled change hover color to green
+        // if scout is disabled change hover color to red
+        //class="bg-success py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        if (scout.enabled) {
+            scout.hoverColor = "green";
+        } else {
+            scout.hoverColor = "red";
+        }
+
         scoutList = scoutList;
         doPost(scout.name, "/admin/toggle_scout");
     };
@@ -58,8 +69,11 @@
         <input id="name" type="text" bind:value={name} />
     </form>
 </div>
-<div class="grid grid-cols-4 auto-rows-auto gap-y-2 gap-x-64">
+<!-- <div class="grid grid-cols-4 auto-rows-auto gap-y-2 gap-x-44"> -->
+    <div class="flex flex-wrap w-screen fixed">
+
     {#each scoutList as scout, i}
+
         <!-- {#if i % cols == 0}
                     <tr>
                         {#each Array(cols) as _, j}
@@ -78,22 +92,38 @@
                                     </button>
                                 </td>
                             {/if}
+
                         {/each}
                         
                     </tr>
                 {/if} -->
 
-        <div class="card w-96 bg-success shadow-xl">
-  <div class="card-body">
-    <h2 class="card-title">Card title!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+            <div class="item w-32 h-32">
+            <div class="tooltip" data-tip="Toggle {scout.name}">
+                <button
+                    class:enabled={scout.enabled}
+                    on:click={() => toggle(scout)}
+                    class:hover={scout.hoverColor}
+                    class="w-full h-full "
+
+                >
+                    {scout.name}
+                </button>
+            </div>
+            </div>
+            <!-- <button
+                on:click={() => toggle(scoutList[i])}
+                class:enabled={scoutList[i].enabled}
+                type="button"
+                class="text-center bg-error py-2 px-4 text-sm font-medium text-gray-900 rounded-l-lg border border-gray-200 hover:z-10 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover: dark:focus:ring-blue-500 dark:focus:text-white"
+            >
+                {scout.name}
+            </button> -->
+            
+
     {/each}
 </div>
+<!-- </div> -->
 
 <style>
     div,
@@ -144,3 +174,4 @@
         padding: 10px;
     }
 </style>
+
