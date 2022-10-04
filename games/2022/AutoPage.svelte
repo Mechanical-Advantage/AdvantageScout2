b042074ff48e8ae25557b0d1e95329812c1c73a5
Aryan <aryank2023@gmail.com>
2022-10-04 17:22:47 -0400

    add buttons in AutoPage

---
+++ games/2022/AutoPage.svelte
@@ -0,0 +1,88 @@
+<script>
+</script>
+
+<main>
+    <div class="line">
+        <div class="btn-group">
+            <button class="btn s">+</button>
+            <button class="btn n">0</button>
+            <button class="btn s">-</button>
+        </div>
+    </div>
+
+    <div class="line">
+        <div class="btn-group">
+            <button class="btn f">+</button>
+            <button class="btn n">0</button>
+            <button class="btn f">-</button>
+        </div>
+    </div>
+
+    <div class="dropdown">
+        <label tabindex="0" class="btn m-1 d">Start Position</label>
+        <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a>Fender</a></li>
+            <li><a>Tarmac</a></li>
+        </ul>
+    </div>
+
+    <br />
+
+    <div class="line">
+        <div class="btn-group">
+            <button class="btn s">+</button>
+            <button class="btn n">0</button>
+            <button class="btn s">-</button>
+        </div>
+    </div>
+
+    <div class="line">
+        <div class="btn-group">
+            <button class="btn f">+</button>
+            <button class="btn n">0</button>
+            <button class="btn f">-</button>
+        </div>
+    </div>
+</main>
+
+<style>
+    button {
+        border: none;
+        color: white;
+        padding-top: 50px;
+        padding-bottom: 50px;
+
+        display: inline-block;
+        font-size: 30px;
+
+        margin-top: 50px;
+        text-align: center;
+        vertical-align: middle;
+        line-height: 0;
+
+        width: 150px;
+
+        margin-left: 25px;
+        margin-right: 25px;
+    }
+
+    .s {
+        background-color: #00ff00;
+    }
+    .f {
+        background-color: #ff0000;
+    }
+
+    .n {
+        background-color: rgb(215, 215, 215);
+    }
+
+    .line {
+        display: inline-block;
+        margin-top: 150px;
+    }
+
+    .d {
+        background-color: gold;
+    }
+</style>
--- games/2022/GameComponent.svelte
+++ games/2022/GameComponent.svelte
@@ -1,7 +1,15 @@
 <script>
+    import NavBar from "./NavBar.svelte";
+    import AutoPage from "./AutoPage.svelte";
 </script>
 
-<main />
+<main>
+    <NavBar />
+    <AutoPage />
+</main>
 
 <style>
+    main {
+        background-color: rgb(50, 50, 50);
+    }
 </style>
