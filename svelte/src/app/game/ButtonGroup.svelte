7c22a93f0d592287f1999943acc687178c1bef57
Aryan <aryank2023@gmail.com>
2022-10-12 19:30:18 -0400

    minor changes

---
+++ svelte/src/app/game/ButtonGroup.svelte
@@ -0,0 +1,34 @@
+<script>
+</script>
+
+<main>
+    <div class>
+        <div class="btn-group">
+            <button class="btn">+</button>
+            <button class="btn">0</button>
+            <button class="btn">-</button>
+        </div>
+    </div>
+</main>
+
+<style>
+    button {
+        border: none;
+        color: white;
+        padding-top: 60px;
+        padding-bottom: 60px;
+
+        display: inline-block;
+        font-size: 30px;
+
+        margin-top: 50px;
+        text-align: center;
+        vertical-align: middle;
+        line-height: 0;
+
+        width: 150px;
+
+        margin-left: 25px;
+        margin-right: 25px;
+    }
+</style>
---
+++ svelte/src/app/game/NavBar.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<main>
+
+    <div class="navbar">
+        <button class="btn btn-active">Auto</button>
+        <button class="btn btn-active">Tele-Op</button>
+        <button class="btn btn-active">End Game</button>
+    </div>
+
+</main>
+
+<style>
+    .navbar {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    button {
+        background-color: rgb(51, 96, 179);
+        color: rgb(255, 255, 255);
+        padding-top: 60px;
+        padding-bottom: 60px;
+        width: 31%;
+        margin-top: 10px;
+        margin-left: 15px;
+        margin-right: 15px;
+        text-align: center;
+        vertical-align: middle;
+        line-height: 0;
+        font-size: 50px;
+    }
+</style>
